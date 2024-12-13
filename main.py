ef3fe4eb0089d1ede3bcc923469d022d99f49610
AleksZieba <aleks@lepszyangielski.pl>
2024-12-13 19:08:52 +0100

    Refactorting.

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ from constants import *
 def main():
     pygame.init() 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
-    loop = True
-    while loop:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000) 
-        #print("Starting asteroids!") 
-        #print(f"Screen width: {SCREEN_WIDTH}")
-        #print(f"Screen height: {SCREEN_HEIGHT}") 
         pygame.display.flip()
 
 if __name__ == "__main__":
