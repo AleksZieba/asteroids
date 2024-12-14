816158bb455d7ab0ee5da8c8238f265d830b0378
AleksZieba <aleks@lepszyangielski.pl>
2024-12-14 14:14:46 +0100

    Instance groups implemented.

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
     x = SCREEN_WIDTH / 2 
     y = SCREEN_HEIGHT / 2
+    
+    updatable = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group() 
+    Player.containers = (updatable, drawable)
+
+# Change the game loop to use the new groups instead of the Player object directly. 
+# In other words, iterate over all "updatables" and .update() them, 
+# then iterate over all "drawables" and .draw() them.
+
     player = Player(x, y, PLAYER_RADIUS)
 
     while True:
@@ -16,13 +25,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for instance in updatable: 
+            instance.update(dt)
+        #player.update(dt)
 
         screen.fill((0,0,0)) 
-        player.draw(screen)
+
+        for instance in drawable: 
+            instance.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()  
 
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main() 
