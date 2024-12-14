221afc755bc8d6963e6cd974ec8bb2b11cd97153
AleksZieba <aleks@lepszyangielski.pl>
2024-12-14 12:25:07 +0100

    Added left 'a' and 'd' key rotation.

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill((0,0,0)) 
         player.draw(screen)
-        pygame.display.flip() 
+        pygame.display.flip()  
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
